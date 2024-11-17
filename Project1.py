1778bc0b3058693e7a9c4d95d77630a7fd0c1a6c
RenataGirleanu <renatagirleanu@icloud.com>
2024-11-17 17:39:28 +0000

    Create Project1.py

---
+++ Project1.py
@@ -0,0 +1,13 @@
+print("RRRR   GGG ")
+print("R   R G   G")
+print("R   R G    ")
+print("RRRR  GGGGG")
+print("R R   G   G")
+print("R  R  G   G")
+print("R   R  GGG ")
+
+
+# Renata Girleanu
+# I love reading!
+
+
